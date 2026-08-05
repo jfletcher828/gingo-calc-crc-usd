62c7e68413baf96bff813f61f179b937c755d12e
jfletcher828 <148658974+jfletcher828@users.noreply.github.com>
2026-08-05 11:38:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const crc = document.getElementById("crc");
 const rate = document.getElementById("rate");
 const usd = document.getElementById("usd");
 
+document.getElementById("clearBtn")
+    .addEventListener("click", () => {
+
+        crc.value = "";
+        usd.value = "";
+        rate.value = "443";
+    });
+
 function calculate() {
 
     const crcVal = parseFloat(crc.value);
